3b0dcbdbc6ed349c072de04d3e49373db80f22fc
drtaha198900-byte <omar.ms.26.2026@gmail.com>
2026-08-04 12:01:08 +0300

    refactor: update module files to remove TypeORM dependencies

--- src/modules/contact/contact.module.ts
+++ src/modules/contact/contact.module.ts
@@ -1,11 +1,8 @@
 import { Module } from '@nestjs/common';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { Contact } from './entities/contact.entity';
 import { ContactService } from './contact.service';
 import { ContactController } from './contact.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Contact])],
   controllers: [ContactController],
   providers: [ContactService],
   exports: [ContactService],
